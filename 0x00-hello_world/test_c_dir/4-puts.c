838eab53b78f5258ef2e7547eee5c50f72ebd313
swalgocoder <gary.d.gao@gmail.com>
2016-10-12 19:18:10 +0000

    a

---
+++ 0x00-hello_world/test_c_dir/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char str1[50];
+  char str2[50];
+
+  strcpy(str1, "\"Programming is like building a multilingual puzzle");
+  puts(str1);
+    
+  return(0);
+}
