7e6f2be46097c9c11ddd609e540a2c1c6fd6b38e
swalgocoder <gary.d.gao@gmail.com>
2016-10-12 22:42:43 +0000

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,19 @@
-/* data type and their sizes */
 #include <stdio.h>
- 
-/* main function */
-int main(void)
+int main()
 {
-  /* declare and initialize variables */
-  char t = 'r'; /* char data type */q
-  int p = 2000; /* positive integer data type */
-  short int q = -120; /* variation */
-  unsigned short int r = 121; /* variation */
-  float s = 21.566578; /* float data type */
-  char t = 'r'; /* char data type */
-  long u = 5678; /* long positive integer data type */
-  unsigned long v = 5678; /* variation */
-  long w = -5678; /* -ve long integer data type */
-  int x = -171; /* -ve integer data type */
-  short y = -71; /* short -ve integer data type */
-  unsigned short z = 99; /* variation */
-  double a = 88.12345; /* double float data type */
-  float b = -3.245823; /* float data type */
-  char Title[50] = "The Making of the Casino Royale";
+  char charType;
+  int integerType;
+  long longType;
+  long long longlongType;
+  float floatType;
+  
+
+  // Sizeof operator is used to evaluate the size of a variable
+  printf("Size of a char: %ld byte\(s)\n",sizeof(charType));
+  printf("Size of an int: %ld byte(s)\n",sizeof(integerType));
+  printf("Size of a long int: %ld byte(s)\n",sizeof(longType));
+  printf("Size of a long long int: %ld byte(s)\n",sizeof(longlongType));
+  printf("Size of a float: %ld byte(s)\n",sizeof(floatType));
+  
+  return 0;
+}
