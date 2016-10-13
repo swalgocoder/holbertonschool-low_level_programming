02e67747b2d776542652a005a27a5ac8cd15c3a4
swalgocoder <gary.d.gao@gmail.com>
2016-10-13 00:11:42 +0000

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-/* C  function main() coded, puts function called inside int main(void) return no value */
+/*
+ *second program in c
+ */
+/**
+ *main - entry point
+ *print a string followed by a new line using puts
+ *Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
