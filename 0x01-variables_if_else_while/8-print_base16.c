61efce1ad814a3b16ba0d918e61cd4d323c8bf14
swalgocoder <gary.d.gao@gmail.com>
2016-10-13 22:20:19 +0000

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - program starts, evaluates and prints
+ *numbers (base 16)
+ *Return: 0
+ */
+	int main(void)
+	{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	putchar(i);
+	for (i = 97; i < 103; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+	}
