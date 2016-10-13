9a5f681dd4358094caf22112fc74a5a3484ddf7d
swalgocoder <gary.d.gao@gmail.com>
2016-10-13 20:47:56 +0000

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,36 +1,34 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - evaluates and prints  a random number in certain ranges
- * 3 ranges 
+ * 3 ranges
  * Return: 0
  */
-
 int main(void)
-{
-  int n;
-  int lastDigit;
+	{
+	int n;
+	int lastDigit;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
   /* your code goes there */
   /* get last digit of num */
 
-  lastDigit = n%10;
+	lastDigit = n % 10;
 
-  if ( lastDigit > 5)
-    {
-      printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-    }
-  else if ( lastDigit == 0)
-    {
-      printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-    }
-  else
-    {
-      printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-    }
-  return (0);
-}
+	if (lastDigit > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	}
+	else if (lastDigit == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	}
+	return (0);
+	}
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+/**
+ * main - evaluates and prints letters in lowercase
+ * excluding q and e
+ * Return: 0
+ */
 int main(void)
 {
-  char i;
+	char i;
 
-  for(i = 'a'; i <= 'z'; i++)
-    {
-      if (i != 'q' && i != 'e')
-      {putchar(i);
-      }
-    }
-  putchar('\n');
-  return (0);
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	if (i != 'q' && i != 'e')
+	{putchar(i);
+	}
+	}
+	putchar('\n');
+	return (0);
 }
