14da2b58125c97055ad67929ccdfb74dca0d402e
swalgocoder <gary.d.gao@gmail.com>
2016-10-17 22:04:27 +0000

    a

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - program starts, evaluates and prints a string
+ * separated by a comma and space
+ * Return: Returns 0
+ */
+int _putchar(char c);
+
+int main(void)
+{
+  _putchar('H');
+  _putchar('o');
+  _putchar('l');
+  _putchar('b');
+  _putchar('e');
+  _putchar('r');
+  _putchar('t');
+  _putchar('o');
+  _putchar('n');
+  _putchar('\n');
+  return (0);
+}
