85d15b1fad5d5353e21932dbcb0726666f350f05
swalgocoder <gary.d.gao@gmail.com>
2016-10-18 17:29:51 +0000

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,28 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
- * print_alphabet_x10() - program starts, evaluates and prints a string
- * Return: 0
+ * print_alphabet_x10 -  program starts, evaluates and prints a string
+ * main - program entry point
+ * _putchar(int char) writes a character.
+ * Return: Returns 0
  */
-void print_alphabet_x10(void)
-{
- 
-  int i = 0 ;
-  char c;
-  while ( i <= 10 )  
-    {
-      c='a';  
-    while ( c <= 'z' )
-      {
-      _putchar(c);
-      c++;
-       }
-   _putchar('\n');
-   i++;
-    }
-}
+
+	void print_alphabet_x10(void)
+	{
+	int i = 1;
+	char c;
+
+	while (i <= 10)
+	{
+	c = 'a';
+
+	while (c <= 'z')
+	{
+	_putchar(c);
+	c++;
+	}
+
+	_putchar('\n');
+	i++;
+	}
+	}
