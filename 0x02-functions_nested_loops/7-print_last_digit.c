fcaa00e0ee0b310af793e63c24e384b76400996a
swalgocoder <gary.d.gao@gmail.com>
2016-10-18 21:27:15 +0000

    a

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- * _abs - evaluates and prints absolute value of an int
+ * print_last_digit - prints the last digit of any int
  * @c: - int to be evaluated
- * c *= -1 - c=c*(-1), assignment operator
- * Return: Absolute value of arg
- * '0' + r is the same as 48 + r.
- * When r = 0, the expression evaluates to 48 so a 0 is outputted
- * On the other hand, when r = 1, it evaluates to 49 so a 1 is outputted.
- * In other words, '0' + 1 == '1'
+ * Return: last digit of arg
  */
 
 	int print_last_digit(int c)
@@ -18,8 +13,9 @@
 		n = c % 10;
 
 		if (n < 0)
-		n *= -1;
+			{
+			n *= -1;
+			}
 		_putchar('0' + n);
 		return (n);
-
 	}
