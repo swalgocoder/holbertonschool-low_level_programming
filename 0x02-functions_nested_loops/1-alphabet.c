ae079f0759686156af02bf1ceacc6fbb3ef525f1
swalgocoder <gary.d.gao@gmail.com>
2016-10-18 17:10:45 +0000

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,21 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
- * print_alphabet() - program starts, evaluates and prints a string
-  * Return: 0
+ * print_alphabet -  program starts, evaluates and prints a string
+ * main - program entry point
+ * Description: You are not allowed to include standard libraries.
+ * _putchar(int char) writes a character.
+ * separated by a comma and space
+ * Return: Returns 0
  */
-void print_alphabet(void)
-{
-  char c = 'a';
+	void print_alphabet(void)
+	{
+	char c = 'a';
 
-  while (c <= 'z')
-    {
-      _putchar(c);
-      c++;
-    }
-  _putchar('\n');
-}
+	while (c <= 'z')
+	{
+	_putchar(c);
+	c++;
+	}
+	_putchar('\n');
+	}
