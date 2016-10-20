e3e4b35a44e6030b1d297716788342cd6a1e643b
swalgocoder <gary.d.gao@gmail.com>
2016-10-20 18:00:52 +0000

    a

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *mul(int a, int b) -  multiplies two integers
+ *@a: - integer argument
+ *@b: - integer argument
+ *Return: multiplication value
+ */
+
+int mul(int a, int b)
+	{
+	
+	  return(a*b);		
+
+	}
