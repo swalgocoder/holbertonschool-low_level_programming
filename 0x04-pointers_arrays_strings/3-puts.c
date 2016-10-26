2467fa1bf98d6d6ad294382a32e4a614c87dce81
swalgocoder <gary.d.gao@gmail.com>
2016-10-25 19:25:43 -0700

    Update 3-puts.c

--- 0x04-pointers_arrays_strings/3-puts.c
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -4,14 +4,12 @@
  * @str: first parameter
  * Return: string
  */
-
-
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-	  _putchar(*str);
-	  str++;
+		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
