086876b05cff2dd8191e216c372f70582c9f4f20
swalgocoder <gary.d.gao@gmail.com>
2016-10-25 19:11:56 -0700

    Update 9-strcpy.c

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -6,16 +6,15 @@
  * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
-	{
+{
 	int i;
 
 	i = 0;
-
 	while (src[i] != '\0')
-		{
+	{
 		dest[i] = src[i];
 		i++;
-		}
+	}
 	dest[i] = '\0';
 	return (dest);
-	}
+}
