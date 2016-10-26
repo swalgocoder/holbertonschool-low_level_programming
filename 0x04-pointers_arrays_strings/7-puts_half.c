03f9cf5001d2b959a6e770562f5ccfc73b01ec86
swalgocoder <gary.d.gao@gmail.com>
2016-10-25 19:15:40 -0700

    Create 7-puts_half.c

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * puts_half -  put out half of a string.
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	while (str[i] != '\0')
+		i++;
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i + 1) / 2;
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
