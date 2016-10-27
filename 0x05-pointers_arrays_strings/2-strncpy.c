95082a4f232cf417a4709f2dadfd68ff0f39dbc1
swalgocoder <gary.d.gao@gmail.com>
2016-10-27 19:28:33 +0000

    a

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: dest to be appended 
+ * @src: append src to dest
+ * @n:  at most n elements from src
+ * Return: pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, src_i;
+
+	i = 0;
+	src_i = 0;
+
+
+	while (src_i != n)
+	{
+		dest[i] = src[src_i];
+
+		if (src[src_i] == '\0')
+		{
+			dest[i] = '0';
+			break;
+		}
+	 
+		i++;
+		src_i++;
+		
+	}
+
+	while (i != n)
+	{
+	dest[i++] = '\0';
+	}
+
+	return (dest);
+}
