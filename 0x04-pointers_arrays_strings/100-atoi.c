82449d604cdd27d787e9805063d27381324a1ad4
swalgocoder <gary.d.gao@gmail.com>
2016-10-26 20:06:55 -0700

    Create 100-atoi.c

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * _atoi - converts ascii to an integer
+ *
+ * @s: the input 
+ * Return: the converted integer
+ */
+int _atoi(char *s)
+{
+	int n, sign, result_int;
+
+	sign = 1;
+	result_int = 0;
+	for (n = 0; *(s + n) != '\0'; n++)
+	{
+		if (*(s + n) == '-')
+		{
+			sign *= -1;
+		}
+		if (*(s + n) >= '0' && *(s + n) <= '9')
+		{
+			result_int = 10 * result_int - (*(s + n) - '0');
+		}
+		if (result_int < 0 && (*(s + n) < '0' || *(s + n) > '9'))
+		{
+			break;
+		}
+	}
+	if (sign > 0)
+	{
+		result_int *= -1;
+	}
+	return (result_int);
+}
