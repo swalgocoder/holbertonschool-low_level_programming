4e9b4d726685efe6684690d5f227b94155e39e2c
swalgocoder <gary.d.gao@gmail.com>
2016-10-27 20:11:02 -0700

    Update 6-cap_string.c

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -2,24 +2,24 @@
 
 /**
  * cap_string - capitalizes 1st char of a word in a string
- * @s: pointer to char
+ * @c: pointer to char
  *
  * Return: char pointer
  */
-char *cap_string(char *s)
+char *cap_string(char *c)
 {
 	int i = 0;
 
-	while (s[i])
+	while (c[i])
 	{
-		if ((s[i] >= 'a' && s[i] <= 'z')
-		    && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.'
-			|| s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"'
-			|| s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{'
-			|| s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\n'
-			|| s[i - 1] == '\t' || s[i - 1] == '\0'))
-			s[i] = s[i] - 32;
+		if ((c[i] >= 'a' && c[i] <= 'z')
+		    && (c[i - 1] == ',' || c[i - 1] == ';' || c[i - 1] == '.'
+			|| c[i - 1] == '!' || c[i - 1] == '?' || c[i - 1] == '"'
+			|| c[i - 1] == '(' || c[i - 1] == ')' || c[i - 1] == '{'
+			|| c[i - 1] == '}' || c[i - 1] == ' ' || c[i - 1] == '\n'
+			|| c[i - 1] == '\t' || c[i - 1] == '\0'))
+			c[i] = c[i] - 32;
 		i++;
 	}
-	return (s);
+	return (c);
 }
