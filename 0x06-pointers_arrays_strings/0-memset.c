2804d879bb18067c8bd749917e577654e1a3e166
swalgocoder <gary.d.gao@gmail.com>
2016-10-31 18:24:01 +0000

    a

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/** _memset - fills the first n bytes of the memory the constant byte b
+ * @s: pointer to s
+ * @b: the constant
+ * @n: the first n bytes of the memory
+ *
+ * return:  a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+	{
+	int i;
+
+	i = 0;
+
+	while (i < (int)n)
+		{
+		s[i] = b;
+		i++;
+		
+		}
+
+	return(s);
+
+	}
