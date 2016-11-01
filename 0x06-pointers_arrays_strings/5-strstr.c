e5bab6e689f2915354a5ddcc739b04b34819af50
swalgocoder <gary.d.gao@gmail.com>
2016-11-01 00:00:03 +0000

    a

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,39 +1,32 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * _strstr - the 1st occurrence of the substr needle
- * @haystak: pointer to string
- * @needel: pointer string
- *
- * Return: a pointer to the begining  that matches
+ * _strstr - the first occurence of the substring needle
+ * in the string haystack
+ * @haystack: string 4 searching
+ * @needle: string 4 finding
+ * Return: pointer to the beginning of the located substring
  */
 
+
 char *_strstr(char *haystack, char *needle)
+{
+
+	while (*haystack != '\0')
 	{
+		char *initial_located = haystack;
+		char *str_pattern_2find = needle;
 
-
-	char *str_2_find = needle; 
-	char *where_2_find = haystack;
-
-	int i;
-	int j;
-	int c;
-
-	for (i  = 0; *str_2_find  != '\0'; i++)
-	{
-		c = 0;
-		for (j = 0; accept[j] != '\0'; j++)
+		while (*str_pattern_2find == *haystack && *str_pattern_2find != '\0'
+		       && *haystack != '\0')
 		{
-			if (s[i] == accept[j])
-			{
-			c = 1;
-			}
+			haystack++;
+			str_pattern_2find++;
 		}
-		j = 0;
-		if (c == 1)
-		return (s + i);
-
+		if (*str_pattern_2find == '\0')
+			return (initial_located);
+		haystack = initial_located + 1;
 	}
-
-	return (0);
-	}
+	return ('\0');
+}
