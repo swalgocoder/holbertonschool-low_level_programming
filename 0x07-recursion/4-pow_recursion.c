7cc9739c6d6fc56e33bea6c792d3c85c25177a24
swalgocoder <gary.d.gao@gmail.com>
2016-11-02 22:11:55 +0000

    a

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
  *_pow_recursion - the value of x raised to the power of y
@@ -11,10 +11,7 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	
-	else if (y == 0)
+	if (y == 0)
 		return (1);
-	
-	else
 	return (x * _pow_recursion(x, y - 1));
 }
