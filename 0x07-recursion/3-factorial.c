74dd70e85f3511faaee0569cf02ffb53e9cf1ccf
swalgocoder <gary.d.gao@gmail.com>
2016-11-02 21:43:55 +0000

    a

--- 0x07-recursion/3-factorial.c
+++ 0x07-recursion/3-factorial.c
@@ -8,14 +8,14 @@
 int factorial(int n)
 {
 
-	if ( n < 0)
+	if (n < 0)
 		return (-1);
 
 	else if (n == 0)
 		return (1);
 
 	else if (n > 1)
-		return (n * factorial(n-1));
+		return (n * factorial(n - 1));
 
 	else
 		return (n);
