ff70a97537c2dfb88b32220bd15a66a3b25a1cbb
swalgocoder <gary.d.gao@gmail.com>
2016-11-02 22:01:19 +0000

    a

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *_pow_recursion - the value of x raised to the power of y
+ * @x: value to be raised
+ * @y: to the power of y
+ * Return: raised value
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	
+	else if (y == 0)
+		return (1);
+	
+	else
+	return (x * _pow_recursion(x, y - 1));
+}
