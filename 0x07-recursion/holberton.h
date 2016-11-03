4c2872c8ac4993456e89ed3abeb0f3651b8515d3
swalgocoder <gary.d.gao@gmail.com>
2016-11-02 21:48:14 -0700

    Update holberton.h

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -16,3 +16,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int _strlen_recursion(char *s);
+int check_pal(char *str, int len_s);
