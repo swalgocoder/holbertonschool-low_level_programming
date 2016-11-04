0f8163dd9e7f1985e24e2fcf4a3db22eb547113f
vagrant <vagrant@precise32.(none)>
2016-11-04 21:24:23 +0000

    a

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  number of arguments passed from command line
+ * @argc: number of arguments
+ * @argv: values of arguments
+ * Return: 0 
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	if (argc >= 1)
+	  printf("%d\n", argc - 1);
+	return (0);
+}
---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * main - multiplication of  2 numbers
+ * @argc: number of arguments
+ * @argv: value of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	int mul1;
+	int mul2;
+
+	mul1 = 0; 
+	mul2 = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (*argv[1] != '\0')
+	{
+		mul1 = atoi(argv[1]);
+	}
+
+	if (*argv[2] != '\0')
+	{
+		mul2 = atoi(argv[2]);
+	}
+
+	printf("%d\n", mul1 * mul2);
+	return (0);
+}
