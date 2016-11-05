b024cb4fe361ff5b5d2ce74a13fe5e79ac3b3571
swalgocoder <gary.d.gao@gmail.com>
2016-11-04 18:46:48 -0700

    Update 4-add.c

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -10,33 +10,6 @@
  * Return: 0
  */
 
-int main(int argc, char **argv)
-{
-	int sum, val, i;
-
-	sum = 0;
-	if (argc < 1)
-		printf("%d\n", 0);
-
-	i = 0;
-
-	while (argc-- && argc > 0)
-	{
-		while (argv[argc][i] != '\0')
-		{
-
-			if (!(isdigit(argv[argc][i])))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		i++;
-		}
-
-		val = atoi(argv[argc]);
-
-		sum += val;
-	}
 
 	printf("%d\n", sum);
 	return (0);
