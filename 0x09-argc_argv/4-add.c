b745ed89fc87e7c55d07251bd48b3f25b1f60aa1
swalgocoder <gary.d.gao@gmail.com>
2016-11-05 22:45:46 -0700

    Update 4-add.c

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -15,8 +15,10 @@ int ck_input(int argc, int i, unsigned int j, char *argv[])
 	for (i = 1; i <= argc; i++)
 	{
 		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
+		{
 			if (isdigit(argv[i][j]) == 0)
-				return (1);
+			return (1);
+		}
 	}
 	return (0);
 }
