cefc852d16a7378f282fd4ee3a6faf39e58d72a4
swalgocoder <gary.d.gao@gmail.com>
2016-11-05 22:28:54 -0700

    Update 4-add.c

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -15,14 +15,14 @@
 int mydigit(int argc, int i, unsigned int j, char *argv[])
 {
 	for (i = 1; i <= argc; i++)
-		{
+	{
 		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-			{
-			if (isdigit(argv[i][j]) == 0)
-			return (1);
-			}
+		{
+		if (isdigit(argv[i][j]) == 0)
+		return (1);
 		}
-	return (0);
+	}
+		return (0);
 }
 
 /**
@@ -34,20 +34,20 @@ int mydigit(int argc, int i, unsigned int j, char *argv[])
 
 int main(int argc, char *argv[])
 {
-	int sum;
 	int i;
+	int sum;
 
 	sum = 0;
 
-	if (mydigit(argc, 1, 0, argv) == 1)
+	for (i = 1, sum = 0; i < argc; i++)
 	{
+		if (mydigit(argc, 1, 0, argv) == 1)
+		{
 		printf("Error\n");
 		return (1);
-	}
+		}
 	
-	for (i = 1; i < argc; i++)
-	{
-	sum += atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
 	
 	printf("%d\n", sum);
