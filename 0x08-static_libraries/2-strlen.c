6636630e1a9cd60998fe63a5ea1181720b296849
swalgocoder <gary.d.gao@gmail.com>
2016-11-06 00:37:15 -0700

    Update 2-strlen.c

--- 0x08-static_libraries/2-strlen.c
+++ 0x08-static_libraries/2-strlen.c
@@ -1,16 +1,15 @@
-#include "holberton.h"
+#include <unistd.h>
 /**
- * _strlen - returns the length of a string
- *
- * @s: a given string
- * Return: the length of the string
+ * _strlen - check the code for Holberton School students.
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
  */
-
 int _strlen(char *s)
 {
-	int n;
+	int c;
 
-	for (n = 0; *s != '\0'; s++)
-		n++;
-	return (n);
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
 }
