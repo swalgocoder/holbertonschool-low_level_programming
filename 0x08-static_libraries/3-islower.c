48cbef3a31200526cc3935014d72d2482d186671
swalgocoder <gary.d.gao@gmail.com>
2016-11-06 00:40:55 -0700

    Update 3-islower.c

--- 0x08-static_libraries/3-islower.c
+++ 0x08-static_libraries/3-islower.c
@@ -1,18 +1,15 @@
 #include "holberton.h"
 /**
- * _islower - checks for lowercase character
- * @c: integer taken as parameter
- *
- * Return: 1 if lowercase, 0 otherwise
+ * _islower - function to check for lowercase character
+ * @c:  is the int that will use for the argument of the function
+ * Return: 0
  */
 int _islower(int c)
 {
-	if (c >= 99 && c <= 122)
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
 	else
-	{
 		return (0);
-	}
 }
