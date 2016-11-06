67ae87e911f5f3babda7fdd729012f7f91a09239
swalgocoder <gary.d.gao@gmail.com>
2016-11-05 20:53:10 -0700

    Update 2-args.c

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - arguments passed from command line
  * @argc: number of arguments
  * @argv: values of arguments
- * Return: 0 
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int i;
-
 
 		for (i = 0; i < argc; i++)
 		{
