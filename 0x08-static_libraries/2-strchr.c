fffc7f0ec42b4eddbe407cd84b664d1ea60ae3c5
swalgocoder <gary.d.gao@gmail.com>
2016-11-06 00:35:50 -0700

    Update 2-strchr.c

--- 0x08-static_libraries/2-strchr.c
+++ 0x08-static_libraries/2-strchr.c
@@ -1,35 +1,12 @@
-#include "holberton.h"
+#include <unistd.h>
 /**
- * _strlen - string length helper
- *
- * @s: pointer to string
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int n;
-
-	for (n = 0; *s != '\0'; s++)
-		n++;
-	return (n);
-}
-/**
- * _strchr - checks for a char in a string
- * @s: the string
- * @c: the char being searched for
- * Return: pointer to char on success, NULL on failure
+ * _strchr - returns a pointer to the first occurence of the
+ * character c in the string s
+ * @c: parameter for c to return
+ * @s: string to search character
+ * Return: pointer to first occurence of character c or null if not found
  */
 char *_strchr(char *s, char c)
 {
-	int len, n;
-
-	if (*s == '\0')
-		return (0);
-	len = _strlen(s);
-	for (n = 0; n <= len; n++)
-	{
-		if (*(s + n) == c)
-			return (s + n);
-	}
-	return (s + n);
+	return (s);
 }
