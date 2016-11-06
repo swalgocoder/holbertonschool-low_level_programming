f02e5f779574dc3e26159d5a0bf436287b7a8b7b
swalgocoder <gary.d.gao@gmail.com>
2016-11-06 00:29:35 -0700

    Update 1-isdigit.c

--- 0x08-static_libraries/1-isdigit.c
+++ 0x08-static_libraries/1-isdigit.c
@@ -1,15 +1,10 @@
-#include "holberton.h"
-
+#include <unistd.h>
 /**
- * _isdigit - determines if input is uppercase char
- * @c: a single integer input
- * Return: 1 if digit, 0 otherwise
+ * _isdigit - function that checks for uppercase character.
+ * @c: the int for the paramaters of my function
+ * Return: 0
  */
 int _isdigit(int c)
 {
-	if (c <= '9' && c >= '0')
-	{
-		return (1);
-	}
 	return (0);
 }
