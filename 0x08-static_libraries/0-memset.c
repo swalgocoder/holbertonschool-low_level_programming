d68c6a3b09b9c556ab6eb9018cadd8684e08687e
swalgocoder <gary.d.gao@gmail.com>
2016-11-06 00:24:36 -0700

    Update 0-memset.c

--- 0x08-static_libraries/0-memset.c
+++ 0x08-static_libraries/0-memset.c
@@ -1,19 +1,13 @@
-#include "holberton.h"
+#include <unistd.h>
+
 /**
- * _memset - fills a space in memory with b
- *
- * @n: number of bytes to overwrite
- * @s: pointer to the first byte in memory
- * @b: byte for replacement
- * Return: pointer to first byte in memory
+ * _memset - function that fills memory with a constant byte.
+ * @b: b character
+ * @s: s character
+ * @n: int n
+ * Return: On success 1.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
 	return (s);
 }
