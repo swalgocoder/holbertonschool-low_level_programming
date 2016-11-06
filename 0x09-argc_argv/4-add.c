226d4045080081d2d183d093c9e8e18b5089338c
swalgocoder <gary.d.gao@gmail.com>
2016-11-05 22:58:23 -0700

    Update 4-add.c

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -33,18 +33,13 @@ int main(int argc, char *argv[])
 	int sum, i;
 
 	sum = 0;
-
 	if (ck_input(argc, 1, 0, argv) == 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
 	for (i = 1; i < argc; i++)
-		{
 		sum += atoi(argv[i]);
-		}
-	
 	printf("%d\n", sum);
 	return (0);
 }
