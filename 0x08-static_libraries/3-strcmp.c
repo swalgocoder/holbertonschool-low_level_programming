d17519be29c38da3265176a6c965748898afd27d
swalgocoder <gary.d.gao@gmail.com>
2016-11-06 00:44:36 -0700

    Update 3-strcmp.c

--- 0x08-static_libraries/3-strcmp.c
+++ 0x08-static_libraries/3-strcmp.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 /**
- * _strcmp - compares two strings and returns their difference
- *
- * @s1: the first string
- * @s2: the second string
- * Return: 0 if match, s1-s2 otherwise
+ * _strcmp - function that compares two strings
+ * @s2: paramater for string 2
+ * @s1: paramater for string 1
+ * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int n;
+	int a = 0;
 
-	for (n = 0; *(s1 + n) == *(s2 + n); n++)
+	while (s1[a] != '\0')
 	{
-		if (*(s1 + n) == '\0')
-		{
-			return (0);
-		}
+		if (s1[a] < s2[a])
+			return (s1[a] - s2[a]);
+		if (s1[a] > s2[a])
+			return (s1[a] - s2[a]);
+		a++;
 	}
-	return (*(s1 + n) - *(s2 + n));
+	return (0);
 }
