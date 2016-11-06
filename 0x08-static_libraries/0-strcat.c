20153fd681153102003705bc3cffec33e0616fc6
swalgocoder <gary.d.gao@gmail.com>
2016-11-06 00:27:56 -0700

    Update 0-strcat.c

--- 0x08-static_libraries/0-strcat.c
+++ 0x08-static_libraries/0-strcat.c
@@ -1,36 +1,11 @@
-#include "holberton.h"
+#include <unistd.h>
 /**
- * _strlen - calculates and returns the length of a string
- *
- * @s: input, pointer to a string/char array
- * Return: the string's length
- */
-
-int _strlen(char *s)
-{
-	int n;
-
-	for (n = 0; *s != '\0'; s++)
-		n++;
-	return (n);
-}
-/**
- * _strcat - uses loops and _strlen to cat two strings
- *
- * @src: gets copied to dest
- * @dest: becomes the two strings combined
- * Return: a pointer to dest
+ * _strcat -  function that concatenates two strings.
+ * @src: parameter to a src
+ * @dest: parameter to append
+ * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int srclen = _strlen(src);
-	int destlen = _strlen(dest);
-
-	while (i <= srclen)
-	{
-		*(dest + destlen + i) = *(src + i);
-		i++;
-	}
 	return (dest);
 }
