a6431a0491bb33fb69826e8ef5fa8601de408757
swalgocoder <gary.d.gao@gmail.com>
2016-11-05 20:51:03 -0700

    Update 1-args.c

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
 /**
- * main -  number of arguments passed from command line
+ * main - number of arguments passed from command line
  * @argc: number of arguments
  * @argv: values of arguments
- * Return: 0 
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	(void)argv;
 	if (argc >= 1)
-	  printf("%d\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
