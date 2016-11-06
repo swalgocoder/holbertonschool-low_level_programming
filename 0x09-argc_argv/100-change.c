c102d4cd89df0257913d9de7052767a5e2e110a9
swalgocoder <gary.d.gao@gmail.com>
2016-11-06 10:43:06 -0800

    Update 100-change.c

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -11,43 +11,44 @@
 
 int main(int argc, char **argv)
 {
-	int value, count;
+	int sum;
+	int count;
 
 	count = 0;
-	if (argc != 2)
+	while (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	value = atoi(argv[1]);
-	if (value < 0)
+	sum = atoi(argv[1]);
+	while (sum < 0)
 	{
 		printf("%d\n", 0);
 		return (0);
 	}
-	if (value % 25 >= 0)
+	while (sum % 25 >= 0)
 	{
-		count += value / 25;
-		value = value % 25;
+		count += sum / 25;
+		sum = sum % 25;
 	}
-	if (value % 10 >= 0)
+	while (sum % 10 >= 0)
 	{
-		count += value / 10;
-		value = value % 10;
+		count += sum / 10;
+		sum = sum % 10;
 	}
-	if (value % 5 >= 0)
+	while (sum % 5 >= 0)
 	{
-		count += value / 5;
-		value = value % 5;
+		count += sum / 5;
+		sum = sum % 5;
 	}
-	if (value % 2 >= 0)
+	while (sum % 2 >= 0)
 	{
-		count += value / 2;
-		value = value % 2;
+		count += sum / 2;
+		sum = sum % 2;
 	}
-	if (value % 1 >= 0)
+	while (sum % 1 >= 0)
 	{
-		count += value / 1;
+		count += sum / 1;
 	}
 	printf("%d\n", count);
 	return (0);
