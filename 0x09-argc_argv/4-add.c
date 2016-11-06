e799248fd82998831b47f22a3da2c1dd7c753a0b
swalgocoder <gary.d.gao@gmail.com>
2016-11-05 22:44:17 -0700

    Update 4-add.c

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -13,16 +13,18 @@
 int ck_input(int argc, int i, unsigned int j, char *argv[])
 {
 	for (i = 1; i <= argc; i++)
+	{
 		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
 			if (isdigit(argv[i][j]) == 0)
 				return (1);
+	}
 	return (0);
 }
 /**
- * main - adds all arguments together if they are digits.
- * @argc: argument count only accepts ints separated by spaces.
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
+ * main - adds digits
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
