6436d39699430d834cd312e701a5def457a1396b
swalgocoder <gary.d.gao@gmail.com>
2016-11-05 21:46:01 -0700

    Update holberton.h

--- 0x09-argc_argv/holberton.h
+++ 0x09-argc_argv/holberton.h
@@ -18,4 +18,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int _mydigit(int argc, int i, unsigned int j, char *argv[]);
+int mydigit(int argc, int i, unsigned int j, char *argv[]);
