459712a34752d0edb823543e58f040d96ae2e162
swalgocoder <gary.d.gao@gmail.com>
2016-11-06 00:23:23 -0700

    Update 0-isupper.c

--- 0x08-static_libraries/0-isupper.c
+++ 0x08-static_libraries/0-isupper.c
@@ -1,15 +1,11 @@
-#include "holberton.h"
+#include <unistd.h>
 
 /**
- * _isupper - determines if input is uppercase char
- * @c: a single integer input
- * Return: 1 if uppercase, 0 otherwise
+ * _isupper - writes the character c to stdout
+ * @c: The character to print
+ * Return: 0
  */
 int _isupper(int c)
 {
-	if (c <= 'Z' && c >= 'A')
-	{
-		return (1);
-	}
-	return (0);
+	return 0;
 }
