1213a4d84b0b03bfe75f50f99c57a779f6f85755
swalgocoder <gary.d.gao@gmail.com>
2016-11-05 20:41:38 -0700

    Update 0-whatsmyname.c

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - the name of the executable
  * @argc: number of arguments
@@ -9,6 +8,6 @@
 int main(int argc, char *argv[])
 {
 	if (argc >= 1)
-	  printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
