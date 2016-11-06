b83b0eed8baf4bab5b37175634346030966e398e
swalgocoder <gary.d.gao@gmail.com>
2016-11-05 22:37:21 -0700

    Update 4-add.c

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -2,52 +2,40 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
- * mydigit - checks input
- * @argc: number of argument
- * @i: number of arguments
- * @j: # of element of argument
- * @argv: pointer to argument matrix
- * Return: 1 for failure else 0
+ * checker - checks for valid input
+ * @argc: argument count
+ * @i: counter for argv[]
+ * @j: counter for argv[][]
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
  */
-
-int mydigit(int argc, int i, unsigned int j, char *argv[])
+int checker(int argc, int i, unsigned int j, char *argv[])
 {
 	for (i = 1; i <= argc; i++)
-	{
 		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-		{
-		if (isdigit(argv[i][j]) == 0)
-		return (1);
-		}
-	}
-		return (0);
+			if (isdigit(argv[i][j]) == 0)
+				return (1);
+	return (0);
 }
-
 /**
- * main - adds all digits
- * @argc: number of argument
- * @argv: pointer argument matrix
- * Return: 0 on success
+ * main - adds all arguments together if they are digits.
+ * @argc: argument count only accepts ints separated by spaces.
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
  */
-
 int main(int argc, char *argv[])
 {
-	int i;
-	lnt sum;
-	
-	sum = 0;
-	
-	if (mydigit(argc, 1, 0, argv) == 1)
+	int result, i;
+
+	result = 0;
+	if (checker(argc, 1, 0, argv) == 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
 	for (i = 1; i < argc; i++)
-		sum += atoi(argv[i]);
-	
-	printf("%d\n", sum);
+		result += atoi(argv[i]);
+	printf("%d\n", result);
 	return (0);
 }
