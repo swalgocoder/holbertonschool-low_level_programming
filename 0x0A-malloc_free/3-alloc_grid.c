daeaf33db4d35ddb069b67b0fd2def8bdeabe23a
swalgocoder <gary.d.gao@gmail.com>
2016-11-08 20:19:02 -0800

    Update 3-alloc_grid.c

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -7,29 +7,29 @@
  *
  * Return: Pointer to  array, NULL if it fails
  */
+
 int **alloc_grid(int width, int height)
 {
-	int **mygrid;
 	int count, count2;
+	int **my_grid;
 
-        if (width <= 0 || height <= 0)
-	  {
-	    return (NULL);
-	  }
-
-	mygrid = malloc(height * sizeof(int *));
+	count = count2 = 0;
+	my_grid = (int **)malloc(height * sizeof(my_grid));
+	if (my_grid == NULL)
+		return (NULL);
 
 	for (count = 0; count < height; count++)
 	{
-		mygrid[count] = malloc(width * sizeof(int));
-		if (mygrid[count] == NULL)
-			return (NULL);
-
-		for (count2 = 0; count2 < width; count2++)
+		my_grid[count] = malloc(width * sizeof(int *));
+		if (my_gid[count] == NULL)
 		{
-			mygrid[count][count2] = 0;
+			for (count2 = 0; count2 < count; count2++)
+				free(my_grid[count2]);
+			free(my_grid);
+			return (NULL);
 		}
-
+		for (count2 = 0; count2 < width; count2++)
+			my_grid[count][cout2] = 0;
 	}
-	return (mygrid);
+	return (my_grid);
 }
