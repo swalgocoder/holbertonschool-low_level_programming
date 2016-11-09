61a984e77a2e01e8906e79a700a45a7df033b6a4
swalgocoder <gary.d.gao@gmail.com>
2016-11-08 20:49:56 -0800

    Update 3-alloc_grid.c

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -16,7 +16,9 @@ int **alloc_grid(int width, int height)
 	mygrid = malloc(height * sizeof(int *));
 	if (mygrid == NULL)
 		return (NULL);
-	for (count = 0; count < height; count++)
+	count = 0;
+	
+	while (count < height)
 	{
 		mygrid[count] = malloc(width * sizeof(int *));
 		if (mygrid[count] == NULL)
@@ -30,9 +32,17 @@ int **alloc_grid(int width, int height)
 			free(mygrid);
 			return (NULL);
 		}
+	count++;
 	}
-	for (count = 0; count < height; count++)
+	
+	count = 0;
+	while (count < height)
+	{
 		for (count2 = 0; count2 < width; count2++)
+		{
 			mygrid[count][count2] = 0;
+		}
+	count++;
+	}
 	return (mygrid);
 }
