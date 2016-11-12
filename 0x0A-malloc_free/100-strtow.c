258f38d7438e25bae698355bd844b89ed88024cf
swalgocoder <gary.d.gao@gmail.com>
2016-11-12 09:37:49 -0800

    Update 100-strtow.c

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -9,9 +9,9 @@
 char **strtow(char *str)
 {
 	char **myarray;
-	int count1, count2, w_count, len, k, count3;
+	int count1, count2, w_count, len, array_row, count3;
 
-	w_count = k = 0;
+	w_count = array_row = 0;
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
 	for (count1 = 0; str[count1] != '\0'; count1++)
@@ -24,7 +24,7 @@ char **strtow(char *str)
 	myarray = malloc(((w_count + 1) * sizeof(char *)));
 	if (myarray == NULL)
 		return (NULL);
-	for (count1 = 0; str[count1] !=  '\0' && k < w_count; count1++)
+	for (count1 = 0; str[count1] !=  '\0' && array_row < w_count; count1++)
 	{
 		if (str[count1] != ' ')
 		{
@@ -32,19 +32,19 @@ char **strtow(char *str)
 			count2 = count1;
 			while (str[count2] != ' ' && str[count2] != '\0')
 				count2++, len++;
-			myarray[k] = malloc((len + 1) * sizeof(char));
-			if (myarray[k] == NULL)
+			myarray[array_row] = malloc((len + 1) * sizeof(char));
+			if (myarray[array_row] == NULL)
 			{
-				for (k = k - 1; k >= 0; k++)
-					free(myarray[k]);
+				for (array_row = array_row - 1; array_row >= 0; array_row++)
+					free(myarray[array_row]);
 				free(myarray);
 				return (NULL);
 			}
 			for (count3 = 0; count3 < len; count3++, count1++)
-				myarray[k][count3] = str[count1];
-			myarray[k++][count3] = '\0';
+				myarray[array_row][count3] = str[count1];
+			myarray[array_row++][count3] = '\0';
 		}
 	}
-	myarray[k] = NULL;
+	myarray[array_row] = NULL;
 	return (myarray);
 }
