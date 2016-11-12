b2d3efef86e943f33bdf06112a96c6626f412186
swalgocoder <gary.d.gao@gmail.com>
2016-11-11 20:26:14 -0800

    Create holberton.h

---
+++ 0x0A-malloc_free/holberton.h
@@ -0,0 +1,13 @@
+char *create_array(unsigned int size, char c);
+/* task 1 */
+char *_strdup(char *str);
+/* task 2 */
+char *str_concat(char *s1, char *s2);
+/* task 3 */
+int **alloc_grid(int width, int height);
+/* task 4*/
+void free_grid(int **grid, int height);
+/* task 5 */
+char *argstostr(int ac, char **av);
+/* task 6 */
+char **strtow(char *str);
