da76469262bd4d86eac7ee24c970dcbe922772ba
swalgocoder <gary.d.gao@gmail.com>
2016-11-11 20:30:43 -0800

    Update 100-strtow.c

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -1,75 +1,52 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
 
 /**
- * num_words - helper function to find number of words
- * @temp_val: string
- * Return: value
+ * strtow - splits strings to words
+ * @str: string to split
+ * Return: pointer to an array of words
  */
-int num_words(char *temp_val)
-{
-	int i;
 
-	i = 0;
-	while (*temp_val != '\0')
-	{
-		if (*temp_val == ' ')
-			temp_val++;
-		else
-		{
-			while (*temp_val != ' ' && *temp_val != '\0')
-				temp_val++;
-			i++;
-		}
-	}
-	return (i);
-}
-
-/**
- * strtow - convert string to array of words
- * @str: string input
- * Return: array of words
- */
 char **strtow(char *str)
 {
-	int i, j, word_num, word_len;
-	char *temp_word, **word_array;
+	char **array;
+	int i, j, count, len, k, m;
 
-	i = j = word_num = word_len = 0;
-	if (str == NULL || *str == '\0')
+	count = k = 0;
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
-	word_num = num_words(str);
-	if (word_num == 0)
-		return (NULL);
-	word_array = malloc((word_num + 1) * sizeof(char *));
-	if (word_array == NULL)
-		return (NULL);
-	while (*str != '\0' && i < word_num)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (*str == ' ')
-			str++;
-		else
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			count++;
+	}
+	if (count == 0)
+		return (NULL);
+	array = malloc(((count + 1) * sizeof(char *)));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; str[i] !=  '\0' && k < count; i++)
+	{
+		if (str[i] != ' ')
 		{
-			temp_word = str;
-			while (*str != ' ' && *str != '\0')
-				word_len++, str++;
-			word_array[i] = malloc((word_len + 1) * sizeof(char));
-			if (word_array[i] == NULL)
+			len = 0;
+			j = i;
+			while (str[j] != ' ' && str[j] != '\0')
+				j++, len++;
+			array[k] = malloc((len + 1) * sizeof(char));
+			if (array[k] == NULL)
 			{
-				for (i = i - 1; i >= 0; i++)
-					free(word_array[i]);
-				free(word_array);
+				for (k = k - 1; k >= 0; k++)
+					free(array[k]);
+				free(array);
 				return (NULL);
 			}
-			while (*temp_word != ' ' && *temp_word != '\0')
-			{
-				word_array[i][j] = *temp_word;
-				temp_word++; j++;
-			}
-			word_array[i][j] = '\0';
-			i++; j = 0; word_len = 0; str++;
+			for (m = 0; m < len; m++, i++)
+				array[k][m] = str[i];
+			array[k++][m] = '\0';
 		}
 	}
-	word_array[word_num] = NULL;
-	return (word_array);
+	array[k] = NULL;
+	return (array);
+}
