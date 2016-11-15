a8deeabc2324d2db812311f9fa632248e46127a1
swalgocoder <gary.d.gao@gmail.com>
2016-11-14 19:53:50 -0800

    Update 4-new_dog.c

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,39 +2,39 @@
 #include "dog.h"
 
 /**
- * new_dog - create a new dog!!
- * @name: name of the new dog
- * @age: age of the new dog
- * @owner: lucky owner of new dog
- * Return: the dog_t struct
+ * new_dog - pointer to new dog
+ * @name: pointer
+ * @age: age, float
+ * @owner: pinter owner
+ * Return: pointer the dog_t struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *doggo;
+	dog_t *new_dog;
 	int i;
 	char *temp;
 
-	doggo = malloc(sizeof(dog_t));
-	if (doggo == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 		return (NULL);
 	temp = name;
 	for (i = 0; *temp != '\0'; i++, temp++)
 		;
 	if (i == 0)
 		return (NULL);
-	doggo->name = malloc(sizeof(char) * i);
-	if (doggo->name == NULL)
+	new_dog->name = malloc(sizeof(char) * i);
+	if (new_dog->name == NULL)
 		return (NULL);
-	doggo->name = name;
-	doggo->age = age;
+	new_dog->name = name;
+	new_dog->age = age;
 	temp = owner;
 	for (i = 0; *temp != '\0'; i++, temp++)
 		;
 	if (i == 0)
 		return (NULL);
-	doggo->owner = malloc(sizeof(char) * i);
-	if (doggo->owner == NULL)
+	new_dog->owner = malloc(sizeof(char) * i);
+	if (new_dog->owner == NULL)
 		return (NULL);
-	doggo->owner = owner;
-	return (doggo);
+	new_dog->owner = owner;
+	return (new_dog);
 }
