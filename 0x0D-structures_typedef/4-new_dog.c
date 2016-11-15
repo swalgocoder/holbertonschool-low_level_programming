b8b7b2c741f42358d2c0661152dad760b3632c41
swalgocoder <gary.d.gao@gmail.com>
2016-11-14 20:03:43 -0800

    Update 4-new_dog.c

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -11,28 +11,31 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int i;
+	int count;
 	char *swap;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 	swap = name;
-	for (i = 0; *swap != '\0'; i++, swap++)
-		;
-	if (i == 0)
+	count = 0;
+	while (count = 0; *swap != '\0')
+	{
+	count++, swap++;
+	}
+	if (count == 0)
 		return (NULL);
-	new_dog->name = malloc(sizeof(char) * i);
+	new_dog->name = malloc(sizeof(char) * count);
 	if (new_dog->name == NULL)
 		return (NULL);
 	new_dog->name = name;
 	new_dog->age = age;
 	swap = owner;
-	for (i = 0; *swap != '\0'; i++, swap++)
+	for (count = 0; *swap != '\0'; count++, swap++)
 		;
-	if (i == 0)
+	if (count == 0)
 		return (NULL);
-	new_dog->owner = malloc(sizeof(char) * i);
+	new_dog->owner = malloc(sizeof(char) * count);
 	if (new_dog->owner == NULL)
 		return (NULL);
 	new_dog->owner = owner;
