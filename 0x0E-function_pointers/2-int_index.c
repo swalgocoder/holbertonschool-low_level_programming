c5fc0b58d1392812e412fef02dfeb311087cbebc
swalgocoder <gary.d.gao@gmail.com>
2016-11-16 22:28:51 -0800

    Update 2-int_index.c

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -15,12 +15,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	swap = cmp;
 	if (size <= 0 || array == NULL || swap == NULL)
 		return (-1);
-
-	cmp_eval = swap(array[count]);
-	if (cmp_eval == 0)
-		for (count = 0; count < size; count++)
-	else
-		return (count);
-
+	count = 0;
+	while (count < size)
+	{
+		cmp_eval = swap(array[count]);
+		if (cmp_eval == 0)
+			count++;
+		else
+			return (count);
+	}
 	return (-1);
 }
+
