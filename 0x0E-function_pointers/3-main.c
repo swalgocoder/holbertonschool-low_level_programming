25284bb4095e90a3545595fe5ac3b5f1e68d3544
swalgocoder <gary.d.gao@gmail.com>
2016-11-17 04:43:26 -0800

    Update 3-main.c

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,36 +1,41 @@
 #include "3-calc.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
- * main - makes use of other fns to perform arithmetic
- *
- * @argc: number of inputs
- * @argv: array of inputs as strings
- * Return: Error or computed number
+ * main - main function
+ * @argc: argument count
+ * @argv: string of arguments in array
+ * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
 	int a, b;
-	int (*fn)(int, int);
+	int (*o)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	fn = (get_op_func(argv[2]));
-	if (fn == NULL)
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+
+	o = get_op_func(argv[2]);
+	if (o == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
-	printf("%d\n", fn(a, b));
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", o(a, b));
+
 	return (0);
 }
