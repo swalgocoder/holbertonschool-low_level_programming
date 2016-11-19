756f7cb254b77de6b593298188ca92c1b4bed13a
vagrant <vagrant@precise32.(none)>
2016-11-19 22:37:56 +0000

    a

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(va_args, int));
 		if (*separator)
-		printf("%s", separator);
+			printf("%s", separator);
 	}
 
 	printf("%d\n", va_arg(va_args, int));
