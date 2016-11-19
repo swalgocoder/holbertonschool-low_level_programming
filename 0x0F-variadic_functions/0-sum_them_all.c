efc03d1319932ad09eec8e7ca57ab6c2805005d4
vagrant <vagrant@precise32.(none)>
2016-11-19 21:48:40 +0000

    a

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum
+ * @n: first argument
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+
+	int sum = 0;
+	va_list va_args;
+	unsigned int i;
+
+	if (n == 0)
+	return (0);
+
+	va_start(va_args, n);
+
+	for (i = 0; i < n; i++)
+	{
+	sum += va_arg(va_args, int);
+	}
+	va_end(va_args);
+	return (sum);
+}
---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,24 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+
+int sum_them_all(const unsigned int n, ...);
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+
+/**
+ * struct printing - struct
+ * @letter: letter
+ * @f: function
+ */
+typedef struct printing
+{
+	char *letter;
+	void (*f)();
+} print_t;
+
+#endif
