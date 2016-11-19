d3223f3bfa66ec40725800ddf3e660a4c86f48be
vagrant <vagrant@precise32.(none)>
2016-11-19 22:46:52 +0000

    a

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -16,14 +16,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(va_args, n);
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(va_args, int));
-		if (separator != NULL)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 
-	printf("%d\n", va_arg(va_args, int));
+	printf("\n");
 
 	va_end(va_args);
 }
