94ca158a7f885bbb30cbd1569aa4e17703b4571b
swalgocoder <gary.d.gao@gmail.com>
2016-11-19 22:45:05 -0800

    Update variadic_functions.h

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,24 +1,18 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
-
-int sum_them_all(const unsigned int n, ...);
-
-void print_numbers(const char *separator, const unsigned int n, ...);
-
-void print_strings(const char *separator, const unsigned int n, ...);
-
-void print_all(const char * const format, ...);
-
+#include <stdarg.h>
 /**
- * struct printing - struct
- * @letter: letter
- * @f: function
+ * struct print_a - a struct
+ * @c: char
+ * @f: pointer to function
  */
-typedef struct printing
+typedef struct print_arg
 {
-	char *letter;
+	char *c;
 	void (*f)();
-} print_t;
-
+} print_arg_t;
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
