a05477888826064c698e28f633cb873e1d124ecc
swalgocoder <gary.d.gao@gmail.com>
2016-11-19 21:06:18 -0800

    Update 2-print_strings.c

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 
-		if (va_arg(va_args, char *) == NULL)
+		if (va_args == NULL)
 		printf("nil");
 		else
 		printf("%s", va_arg(va_args, char *));
