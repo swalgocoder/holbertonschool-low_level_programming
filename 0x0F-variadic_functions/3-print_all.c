921598915aa28a1ef17dde3eeeae4910b4c3a743
swalgocoder <gary.d.gao@gmail.com>
2016-11-20 05:37:31 -0800

    Update 3-print_all.c

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -9,10 +9,11 @@
 void print_string(va_list ptr_arg1, char *separator)
 {
 	char *swap_str;
+	
 	swap_str = (va_list ptr_arg1, char *separator);
 	if (swap_str == NULL)
 		swap_str = "(nil)";
-	printf("%s", swap_str);
+	printf("%s%s", separator, swap_str);
 }
 /**
  *print_int - an int
