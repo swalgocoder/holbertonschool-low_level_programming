35e5b16bc9f44cbe9f393cf6d6952c15f0638a6e
swalgocoder <gary.d.gao@gmail.com>
2016-12-05 22:25:41 -0800

    Update 3-set_bit.c

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
 /**
   * set_bit - sets the value of a bit to 1 at a given index
+  * @index: index position
   * @void: describe argument
   * Return: 0 on success
   */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int bit2add;
+
 	bit2add = 1;
 	bit2add <<= index;
 	if (index > 63)
