48068df60d1d4bfc3508960a62f4f92a5c17e745
swalgocoder <gary.d.gao@gmail.com>
2016-12-05 21:13:18 -0800

    Update holberton.h

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,5 +1,8 @@
 #ifndef HEADER_H
 #define HEADER_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
