9dcf057db20b51b7bd43c629c1c2f62cb8d3ccf4
gary gao <gary.d.gao@gmail.com>
2016-12-05 17:12:35 -0800

    a

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ *binary_to_uint - converts str to int
+ *@b: ptr to stg
+ *Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+	int i;
+
+	i = atoi (b);
+	return(i);
+}
