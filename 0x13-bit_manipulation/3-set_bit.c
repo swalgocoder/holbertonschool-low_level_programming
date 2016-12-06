db748b6ded786e05da6ac0d249b212be83c1ffc2
swalgocoder <gary.d.gao@gmail.com>
2016-12-05 22:14:02 -0800

    Create 3-set_bit.c

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+  * set_bit - sets the value of a bit to 1 at a given index
+  * @void: describe argument
+  * Return: 0 on success
+  */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	n |= (1<<index)
+	return (0);
+}
