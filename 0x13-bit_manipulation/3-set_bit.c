4f841341f38a4621439d97135587b48f8708e81b
swalgocoder <gary.d.gao@gmail.com>
2016-12-05 22:53:45 -0800

    Update 3-set_bit.c

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -7,12 +7,8 @@
   */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int my_num;
-
 	if (index > 64)
 		return (-1);
-
-	my_num = 1 << index;
-	*n = *n | my_num;
+	*n = (*n) | (1 << index);
 	return (1);
 }
