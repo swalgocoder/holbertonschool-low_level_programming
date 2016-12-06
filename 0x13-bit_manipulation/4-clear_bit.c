d2e185e897dd4712f5273a666e6a42910ef6fa5c
swalgocoder <gary.d.gao@gmail.com>
2016-12-05 23:13:11 -0800

    Update 4-clear_bit.c

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * clear_bit - set the value of a bit to 0 at a given index
  * @n: pointer to int
@@ -9,7 +8,7 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 64)
-		return (-1);
+	return (-1);
 	*n &= ~(1 << index);
 	return (1);
 }
