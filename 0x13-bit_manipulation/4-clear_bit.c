607cb883cb44fd6228e3a6b4018bfb23e80db4b1
swalgocoder <gary.d.gao@gmail.com>
2016-12-05 23:19:33 -0800

    Update 4-clear_bit.c

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -2,14 +2,13 @@
 
 /**
  * clear_bit - set the value of a bit to 0 at a given index
- * @n: an int
- * @index: the index where the bit will be changed
- *
- * Return: 1 if it works, -1 if errors
+ * @n: ptr to an int
+ * @index: indexed position
+ * Return: 1 if success, -1 if errors
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(n) * 8)
+	if (index > 64)
 		return (-1);
 	*n = (*n) & (~(1 << index));
 	return (1);
