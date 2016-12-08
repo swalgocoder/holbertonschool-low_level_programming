2c6e451eec610693fa1999c3ebbc71679efa95f0
gary gao <gary.d.gao@gmail.com>
2016-12-07 20:15:52 -0800

    a

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,8 +1,5 @@
-#include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -19,8 +16,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
   char *buffer;
-  int file_opn;
-  size_t file_handle, file_out;
+  int file_opn, error;
+  ssize_t file_in, file_out;
 
 
   buffer = malloc(sizeof(*buffer) * letters);
@@ -39,15 +36,22 @@ if (file_opn == -1)
 
 
 
-file_handle = read(file_opn, buffer, letters);
+file_in = read(file_opn, buffer, letters);
+ if (file_in == -1)
+   return (0);
+ 
+    file_out = write(1, buffer, file_in);
 
-  if (file_handle > 0)
-    file_out = write(1, buffer, file_handle);
+    if (file_out == -1)
+   return (0);
 
 
+error = close(file_opn);
 
-close(file_opn);
-
+ if (error == -1)
+   return (0);
+ 
     free(buffer);
+
     return (file_out);
 }
