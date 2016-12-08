0eae3c732fed2b3ba6f7cec8bb3786e7227a8225
gary gao <gary.d.gao@gmail.com>
2016-12-07 17:20:54 -0800

    a

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,43 +1,40 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "holberton.h"
 #include <unistd.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
+#include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include "holberton.h"
 
 /**
- * read_textfile -  use _putchar to print char array 
- * @filename: pointer to char array or file name
- * @size_t: letter: the actual number of letters
- * Return: the actual number of letters it could read and print
- * if the file can not be opened or read, return 0
- * if filename is NULL return 0
+ * read_textfile - reads a text file
+ * @filename: file to read
+ * @letters: number of letters it should read
+ * Return: actual numbers it could read
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  char *buffer = malloc(sizeof(char) * letters);
-  int file_opn, file_handle, i;
+	int fp, count, i;
+	char *buff;
 
-
-  if (filename == NULL)
-    return (0);
-
-  if((file_opn = open(filename, O_RDWR)) < 0)
-     return (0);
-
-  if ((file_handle = read(file_opn, buffer, letters)) < 0)
-      return (0);
-
-  file_opn = open(filename, O_RDWR);
-  file_handle = read(file_opn, buffer, letters);
-  
-  for (i = 0; i < file_handle; i++)
-    _putchar(buffer[i]);
-
-  close (file_opn);
-      free(buffer);
-  return (i);
+	if (filename == NULL)
+		return (0);
+	/* malloc buffer */
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+	/* open the file and check if fails */
+	fp = open(filename, O_RDWR);
+	if (fp == -1)
+		return (0);
+	/* read files and check if fails */
+	count = read(fp, buff, letters);
+	if (count == -1)
+		return (0);
+	for (i = 0; i < count; i++)
+	{
+		_putchar(buff[i]);
+	}
+	close(fp);
+	free(buff);
+	return (i);
 }
