1e54b529ed038907d370e69d511349298a0b070f
gary gao <gary.d.gao@gmail.com>
2016-12-07 22:37:47 -0800

    a

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,21 +15,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_handle, i;
+  int file_handle, file_proc, i;
 
 	if (filename == NULL)
 		return (-1);
-	if ((file_handle = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR)) == -1)
+	file_handle = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (file_handle == -1)
 	{
 		close(file_handle);
 		return (-1);
 	}
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
+		return (0);
+	}
+
 		for (i = 0; text_content[i] != 0; i++)
 			;
-		write(file_handle, text_content, i);
-	}
+		file_proc = write(file_handle, text_content, i);
+
 	close(file_handle);
+
+	if (file_proc == -1)
+	return (0);
 	return (1);
 }
