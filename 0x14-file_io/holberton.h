148e7aa756a3e396d3d65ce5d30c242004f34a27
gary gao <gary.d.gao@gmail.com>
2016-12-08 15:05:12 -0800

    a

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -3,7 +3,11 @@
 #include <sys/types.h>
 #include <stddef.h>
 #include <stdlib.h>
-
+#include "holberton.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 char *_memset(char *s, char b, unsigned int n);
