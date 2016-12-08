41db94f64491f21f2e44e50fa4a756c224a291d9
gary gao <gary.d.gao@gmail.com>
2016-12-07 21:27:09 -0800

    a

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -9,5 +9,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 char *_memset(char *s, char b, unsigned int n);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *s);
-
+int append_text_to_file(const char *filename, char *text_content);
 #endif
