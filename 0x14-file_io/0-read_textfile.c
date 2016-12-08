43290c95bcc7d3b50aa9e89ca16f343e945f41a7
gary gao <gary.d.gao@gmail.com>
2016-12-07 22:15:16 -0800

    a

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -4,35 +4,29 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include "holberton.h"
 /**
- * read_textfile -  use _putchar to print char array 
+ * read_textfile -  use _putchar to print char array
  * @filename: pointer to char array or file name
- * @size_t: letter: the actual number of letters
+ * @letters: the actual number of letters
  * Return: the actual number of letters it could read and print
  * if the file can not be opened or read, return 0
  * if filename is NULL return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  char *buffer;
-  int file_opn, error;
-  ssize_t file_in, file_out;
+	char *buffer;
+	int file_opn, error;
+	ssize_t file_in, file_out;
 
+	buffer = malloc(sizeof(char) * letters);
 
-  buffer = malloc(sizeof(char) * letters);
-
-  if (buffer == NULL)
-    return (0);
-    
-  if (filename == NULL)
-    return (0);
-
-  file_opn = open(filename, O_RDONLY);
-
-if (file_opn == -1)
-    return (0);
-
+	if (buffer == NULL)
+	return (0);
+	if (filename == NULL)
+	return (0);
+	file_opn = open(filename, O_RDONLY);
+	if (file_opn == -1)
+	return (0);
 	file_in = read(file_opn, buffer, letters);
 	if (file_in == -1)
 	{
@@ -45,9 +39,7 @@ if (file_opn == -1)
 	free(buffer);
 	return (0);
 	}
-
 	error = close(file_opn);
-
 	if (error == -1)
 	{
 	free(buffer);
