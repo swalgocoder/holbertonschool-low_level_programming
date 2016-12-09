6fc11c9298dbc3dceb3f31d147f3de25b261f603
gary gao <gary.d.gao@gmail.com>
2016-12-08 17:46:50 -0800

    a

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "holberton.h"
-
 /**
- * read_textfile - read & write textfile
- * @filename: pointer to file
- * @letters: num of characters to write
- * Return: num of characters written out
+ * read_textfile - read a text file and print it to stdout
+ * @filename: filename to read from
+ * @letters: number of letters to print
+ *
+ * Return: number of letters to print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, i;
-	int in_c = 0, out_c = 0;
-	char *buffer = malloc(sizeof(char) * letters);
+	int fd;
+	ssize_t err, n;
+	char *buf;
+
+	buf = malloc(sizeof(*buf) * letters);
+	if (buf == NULL)
+		return (0);
 
 	if (filename == NULL)
 		return (0);
@@ -21,25 +22,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	if (buffer == NULL)
+	n = read(fd, buf, letters);
+	if (n == -1)
 		return (0);
-
-	while ((in_c = read(fd, buffer, letters)) > 0 && (size_t)out_c < letters)
-	{
-	i = write(1, buffer, (ssize_t)in_c);
-		if (i == -1)
-		{
-			close(fd);
-			free(buffer);
-			return (0);
-		}
-		out_c += i;
-	}
-	i = close(fd);
-	if (in_c == -1)
+	if (n > 0)
+		n = write(STDOUT_FILENO, buf, n);
+	if (n == -1)
 		return (0);
-	if (i == -1)
+	err = close(fd);
+	if (err == -1)
 		return (0);
-	free(buffer);
-	return (out_c);
+	free(buf);
+	return (n);
 }
