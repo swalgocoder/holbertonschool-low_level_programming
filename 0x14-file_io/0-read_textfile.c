010a1608f7148f92f536c80259f9053ebeee1421
gary gao <gary.d.gao@gmail.com>
2016-12-08 17:51:25 -0800

    a

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,37 +1,43 @@
 #include "holberton.h"
+
 /**
- * read_textfile - read a text file and print it to stdout
- * @filename: filename to read from
- * @letters: number of letters to print
+ * read_textfile - reads a text file and prints to POSIX stdout.
  *
- * Return: number of letters to print
+ * @filename: file to be opened and read.
+ * @letters: number of letters it should read and print.
+ * Return: returns the actual number of letters it could read.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t err, n;
+	ssize_t temp, count;
 	char *buf;
 
 	buf = malloc(sizeof(*buf) * letters);
 	if (buf == NULL)
-		return (0);
+		free(buf); return (0);
 
 	if (filename == NULL)
-		return (0);
+		free(buf); return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
+		free(buf); return (0);
 
-	n = read(fd, buf, letters);
-	if (n == -1)
-		return (0);
-	if (n > 0)
-		n = write(STDOUT_FILENO, buf, n);
-	if (n == -1)
-		return (0);
-	err = close(fd);
-	if (err == -1)
-		return (0);
-	free(buf);
-	return (n);
+	count = read(fd, buf, letters);
+	if (count == -1)
+		free(buf); return (0);
+
+	if (count > 0)
+		count = write(STDOUT_FILENO, buf, count);
+
+	if (count == -1)
+		free(buf); return (0);
+
+	temp = close(fd);
+	if (temp == -1)
+	{
+		free(buf); return (0);
+	}
+	free(buf); return (count);
 }
