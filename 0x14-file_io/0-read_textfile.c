d353f6b5b88d294879606ead9e7224fc30f414a8
gary gao <gary.d.gao@gmail.com>
2016-12-08 18:37:44 -0800

    a

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,54 +1,49 @@
 #include "holberton.h"
 /**
- * open_file - open file
- * @filename: pointer to file
- * Return: int
- */
-int open_file(const char *filename)
-{
-	int fd;
-
-	fd = open(filename, O_RDONLY);
-	return (fd);
-}
-/**
- * read_textfile - reads file,prints to POSIX stdout.
+ * read_textfile - read a text file and print it to stdout
+ * @filename: filename to read from
+ * @letters: number of letters to print
  *
- * @filename: filename
- * @letters: number of letters to read and print.
- * Return: returns the number of letters readed.
+ * Return: number of letters to print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t cls_msg, iocount;
-	char *buf = malloc(sizeof(*buf) * letters);
+	ssize_t err, n;
+	char *buf;
 
+	buf = malloc(sizeof(*buf) * letters);
 	if (buf == NULL)
-		free(buf); return (0);
+		return (0);
 
 	if (filename == NULL)
-		free(buf); return (0);
-
-	open_file(filename);
-
+		return (0);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		free(buf); return (0);
-
-	iocount = read(fd, buf, letters);
-	if (iocount == -1)
-		free(buf); return (0);
-
-	if (iocount > 0)
-		iocount = write(STDOUT_FILENO, buf, iocount);
-
-	if (iocount == -1)
-		free(buf); return (0);
-
-	cls_msg = close(fd);
-	if (cls_msg == -1)
 	{
-		free(buf); return (0);
+		free(buf);
+		return (0);
 	}
-	free(buf); return (iocount);
+
+	n = read(fd, buf, letters);
+	if (n == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	if (n > 0)
+		err = write(STDOUT_FILENO, buf, n);
+	if (err < n)
+	{
+		free(buf);
+		return (0);
+	}
+	err = close(fd);
+	if (err == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (n);
 }
