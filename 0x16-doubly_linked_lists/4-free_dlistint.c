e0e1ef54dd155d0819455d2ec442f8c511a429e1
gary gao <gary.d.gao@gmail.com>
2017-01-09 08:52:25 -0800

    a

---
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ *free_dlistint -  adds a new node at the beginning of a dlistint_t list
+ *@head: pointer to head pointer of dlistint_t
+ *Return: void
+ */
+
+
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	current = head;
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+		free(current);
+	}
+
+	free(head);
+}
