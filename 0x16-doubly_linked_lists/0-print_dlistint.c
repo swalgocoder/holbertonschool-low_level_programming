862efee4f19a4435ab7c46f18a99a612381dca30
gary gao <gary.d.gao@gmail.com>
2017-01-08 18:42:02 -0800

    a

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *print_dlistint - prints all the elements of a dlistint_t list
+ *@h: A constant pointer pointing to head
+ *Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
