e99c1fa8dd5a30edfcb11328ccaa269bf456452f
gary gao <gary.d.gao@gmail.com>
2017-01-09 20:43:50 -0800

    a

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,65 +1,53 @@
 #include "lists.h"
 /**
- *insert_dnodeint_at_idx -  get the indexed node of a dlistint_t list
- *@head: pointer to head pointer of dlistint_t
- *@index: integer
- *Return: poiner to indexed node
+ * insert_dnodeint_at_idx - inserts node at an indexed position
+ * @head: ptr to head
+ * @idx: indexed position to insert node
+ * @n: int data of the node to be inserted
+ * Return: ptr to new node
  */
-dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_idx(dlistint_t **head, unsigned int idx, int n)
 {
-  dlistint_t *current, *pre, *new_node;
-	unsigned int count;
+	dlistint_t *new, *current;
+	unsigned int i;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (!head)
 		return (NULL);
-	new_node->n = n;
-	
-	if (head == NULL)
-		return (0);
-
-	count = 1;
-	while (current = head && count < index)
+	if (*head == NULL && idx != 0)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL, new->prev = NULL;
+	if (idx == 0)
 	{
-		current = current->next;
-		pre = current;
-		count += 1;
+		new->next = *head;
+		new->prev = NULL;
+		*head = new;
+		return (new);
 	}
-
-	if (idx == 0 && current != NULL)
+	current = *head;
+	i = 0;
+	while (i < idx - 1)
 	{
-		*h = new_node;
-		new_node->prec = NULL:
-		new_node->next = current;
-		return (new_node);
-	}
-	if (idx == 1 && current != NULL)
-	{
-		pre = *h;
-		current = *h)->next;
-		new_node->prev = pre:
-		new_node->next = current;
-		pre->next = new_node;
-		current->prev = new_node;
-		return (new_node);
-
-	}
-	if (count == idx && current == NULL)
-	{
-		if (pre != NULL)
-			new_node->prev = pre;
-			new_node->next = NULL;
-			pre->next = new_node;
+		if (current->next == NULL)
+		{
+			free(new);
 			return (NULL);
+		}
+		current = current->next;
+		i++;
 	}
-	if (count != idx && current == NULL)
+	if (i != (idx - 1))
+	{
+		free(new);
 		return (NULL);
-	pre = current;
-	current = current->next;
-	new_node->prev = pre;
-	new_node->next = current;
-	pre->next = new_code;
-	current->prev= new_code;
-
-	return (new_node);
+	}
+	new->next = current->next;
+	current->next = new;
+	new->prev = current;
+	if (new->next)
+		(new->next)->prev = new;
+	return (new);
 }
