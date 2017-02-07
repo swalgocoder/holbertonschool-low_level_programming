e55a97cd3db7fad3d433aab07a2177e98ad51b76
gary gao <gary.d.gao@gmail.com>
2017-02-07 10:47:24 -0800

    fixed print

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -66,8 +66,8 @@ void bubble_sort(int *array, size_t size)
       if (array[j] > array[j + 1])
       {
       	swap(&array[j], &array[j + 1]);
+	print_array(array, size);
       } 
     }
-    print_array(array, size);
   }
 }
