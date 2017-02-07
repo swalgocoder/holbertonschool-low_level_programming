e16df0deb72e426d8e075efb00f1f336105c95e8
gary gao <gary.d.gao@gmail.com>
2017-02-07 11:30:21 -0800

    a

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -1,48 +1,10 @@
-#include <stdio.h>
-#include <stddef.h>
-#include <stdlib.h>
-
-void print_array(const int *array, size_t size);
-void swap(int *xp, int *yp);
-
-void bubble_sort(int *array, size_t size);
-
-int main(void)
-{
-
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-	size_t n = sizeof(array) / sizeof(array[0]);
-	
-	print_array(array, n);
-	printf("\n");
-	
-	bubble_sort(array, n);
-	printf("\n");
-	print_array(array, n);
-	return (0);
-}
-
-
+#include "sort.h"
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * swap: swap array elements
+ * @xp: ptr to array element
+ * @yp: prt to array element
+ * Return: void
  */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
 
 void swap(int *xp, int *yp)
 {
@@ -51,12 +13,17 @@ void swap(int *xp, int *yp)
   *yp = temp;
 }
 
-
+/**
+ * bubble_sort - sort ab array of integers asending order
+ * @array: input array
+ * @size: size of array
+ * Return: void
+ */
 
 
 void bubble_sort(int *array, size_t size)
 {
-  size_t i, j, temp;
+  size_t i, j;
   
 
   for (i = 1; i < size; i++)
