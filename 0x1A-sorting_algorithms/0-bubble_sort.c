14d9ddb64cb2191fb183a341f67ddd4b68bac1a9
gary gao <gary.d.gao@gmail.com>
2017-02-06 22:48:44 -0800

    1st try

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -23,15 +23,16 @@ void bubble_sort(int array[], size_t size)
   size_t i, j, k, temp;
   
   printf("\nUnsorted array:");
-  
+   
   for (k = 0; k < size; k++)
   {
 	printf("%5d,", array[k]);
   }
-  
+  //* the 1st iteration move the largest element at the end of the list*// 
   for (i = 1; i < size; i++)
   {
     //* size -1 not to get out of index*//
+    //* the 2nd iteration move the 2nd largest element to the 2nd last position *//
     for (j = 0; j < size - 1; j++) //*last i elements are already in place*//
     {
       if (array[j] > array[j + 1])
