85f26b13caf2067205867c442fad3fab2200be22
gary gao <gary.d.gao@gmail.com>
2017-02-07 08:54:08 -0800

    revision

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+void print_array(const int *array, size_t size);
 void swap(int *xp, int *yp);
 
 void bubble_sort(int *array, size_t size);
@@ -10,13 +11,38 @@ int main(void)
 {
 
 	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-
 	size_t n = sizeof(array) / sizeof(array[0]);
-
+	
+	print_array(array, n);
+	printf("\n");
+	
 	bubble_sort(array, n);
+	printf("\n");
+	print_array(array, n);
 	return (0);
 }
-  
+
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
 
 void swap(int *xp, int *yp)
 {
@@ -26,33 +52,22 @@ void swap(int *xp, int *yp)
 }
 
 
+
+
 void bubble_sort(int *array, size_t size)
 {
-  size_t i, j, k, temp;
+  size_t i, j, temp;
   
-  printf("\nUnsorted array:");
-   
-  for (k = 0; k < size; k++)
-  {
-	printf("%5d,", array[k]);
-  }
-  //* the 1st iteration move the largest element at the end of the list*// 
+
   for (i = 1; i < size; i++)
   {
-    //* size -1 not to get out of index*//
-    //* the 2nd iteration move the 2nd largest element to the 2nd last position *//
-    for (j = 0; j < size - 1; j++) //*last i elements are already in place*//
+    for (j = 0; j < size - 1; j++)
     {
       if (array[j] > array[j + 1])
       {
       	swap(&array[j], &array[j + 1]);
-
       } 
-    }	  
-  }
-  printf("\nAfter pass %lu : ", i);
-  for (k = 0; k < size; k++)
-  {
-  printf("%5d,", array[k]);
+    }
+    print_array(array, size);
   }
 }
