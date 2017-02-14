70aec8a64f15ef470979871767150f788413e295
gary gao <gary.d.gao@gmail.com>
2017-02-14 10:14:03 -0800

    simplified

--- 0x1A-sorting_algorithms_clone/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms_clone/1-insertion_sort_list.c
@@ -6,57 +6,51 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-  listint_t *query, *tmp_head, *tmp, *prev_query;
 
-
-	tmp_head = *list; /**@head represents a linked list **/ 
-	query = (*list)->next;
-
-	/**if tmp_head-> == NULL: there is only one element in list**/
-		
-	while (query && tmp_head->next)
-	{
-	  if (query->n < query->prev->n) /**comparison starts**/
-		{
-			tmp = query->next;
-
-			while (query->prev)
-			  {
-			    prev_query = query->prev;
-			    if (query->n < prev_query->n)
-			      {
-			      swap_left(prev_query, query);
-			      while ((*list)->prev != NULL)
-				*list = (*list)->prev;
-			      print_list(*list);
-			      }
-			    else
-			      break;
-			  }
-			
-			query = tmp;
-		}
-		else
-			query = query->next;
-	}
-}
-
-
-/**
- * swap_left - swap the identified nodes
- * @left_node: the node to the left( before) of  node
- * @right_node: the node to be checked, somewhere in sorted list
- * @list: the head representing the linked list
- */
-void swap_left(listint_t *left_node, listint_t* right_node)
-{
-	if (left_node->prev)
-	  (left_node->prev)->next =right_node;
-	if (right_node->next)
-	  (right_node->next)->prev = left_node;
-	right_node->prev = left_node->prev;
-	left_node->next = right_node->next;
-	right_node->next = left_node;
-	left_node->prev =right_node;
-
-}
+        int n;
+    	listint_t *cur, *ptr,*tmp;
+    	cur = *list;
+    	if (cur->next==NULL)
+    		return;
+    	
+    	cur=cur->next;
+    	while(cur!=NULL)
+    	{
+    		n=0;
+    		ptr=cur;
+    		tmp=cur->prev;
+    		cur=cur->next;
+    		while (tmp!=NULL && tmp->n>ptr->n)
+    		{
+    			n++;
+    			tmp=tmp->prev;
+			while ((*list)->prev != NULL)
+			    *list = (*list)->prev;
+			    print_list(*list);
+    		}
+    		if (n)
+    		{
+    			
+    			ptr->prev->next=ptr->next;
+    			if (ptr->next!=NULL)
+    			ptr->next->prev=ptr->prev;
+    	
+    			if (tmp==NULL)  /* currently compared node is the smalles*/
+    			{
+    				tmp=*list;
+    				ptr->prev=NULL;
+    				ptr->next=tmp;
+    				ptr->next->prev=ptr;
+    				*list=ptr;
+    			}
+    			else  /* currently compared node lies in between somewhere in sorted list*/
+    			{
+    				tmp=tmp->next;
+    				tmp->prev->next=ptr;
+    				ptr->prev=tmp->prev;
+    				tmp->prev=ptr;
+    				ptr->next=tmp;
+    			}
+    		}
+    	}
+    }
