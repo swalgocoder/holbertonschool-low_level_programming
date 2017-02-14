759d5d6666a15d7e67e9e8e932c4af76ed405a21
gary gao <gary.d.gao@gmail.com>
2017-02-13 21:09:28 -0800

    insertion sort

---
+++ 0x1A-sorting_algorithms_clone/1-insertion_sort_list.c
@@ -0,0 +1,61 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - insertion sort
+ * @list: linked list of listint_t list
+ */
+void insertion_sort_list(listint_t **list)
+{
+  listint_t *query, *tmp_head, *tmp, *prev_query;
+
+	tmp_head = *list; /**@head represents a linked list **/ 
+	query = (*list)->next;
+
+	if (tmp_head->next == NULL) /** there is only one element in list**/
+	  return;
+	
+	while (query)
+	{
+	  if (query->n < query->prev->n) /**comparison starts**/
+		{
+			tmp = query->next;
+
+			while (query->prev)
+			  {
+			    prev_query = query->prev;
+			    if (query->n < prev_query->n)
+			      {
+			      swap_left(prev_query, query);
+			      while ((*list)->prev != NULL)
+				*list = (*list)->prev;
+			      print_list(*list);
+			      }
+			    else
+			      break;
+			  }
+			
+			query = tmp;
+		}
+		else
+			query = query->next;
+	}
+}
+
+
+/**
+ * swap_left - swap the identified nodes
+ * @left_node: the node to the left( before) of  node
+ * @right_node: the node to be checked, somewhere in sorted list
+ * @list: the head representing the linked list
+ */
+void swap_left(listint_t *left_node, listint_t* right_node)
+{
+	if (left_node->prev)
+	  (left_node->prev)->next =right_node;
+	if (right_node->next)
+	  (right_node->next)->prev = left_node;
+	right_node->prev = left_node->prev;
+	left_node->next = right_node->next;
+	right_node->next = left_node;
+	left_node->prev =right_node;
+
+}
